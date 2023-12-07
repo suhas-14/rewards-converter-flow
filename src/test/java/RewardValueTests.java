4f5ccf9d8174be9b2540a8e3fcd8f50c9da952f9
suhas-14 <snani4732@gmail.com>
2023-12-07 08:23:37 +0000

    Test cases passed successfully

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,18 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+
+        RewardValue reward = new RewardValue(100.0);
+        int expectedMiles = (int) (100.0/0.0035);
+        int actualMiles = reward.getMilesValue();
+        assertEquals(expectedMiles, actualMiles, "Cash to miles conversion incorrect");
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        RewardValue reward = new RewardValue(35000);
+        double expectedCash =  35000 * 0.0035;
+        double actualCash = reward.getCashValue();
+        assertEquals(expectedCash , actualCash, 0.01, "Miles to cash conversion incorrect");
     }
 }
